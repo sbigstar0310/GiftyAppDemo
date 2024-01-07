30cea0217f00d685041a47a65c226ed403ba0e76
sbigstar0310 <sbigstar0930@gmail.com>
2024-01-07 15:33:58 +0900

    Initial Commit

---
+++ GiftyAppDemo/GiftyAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftyAppDemoApp.swift
+//  GiftyAppDemo
+//
+//  Created by 성대규 on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct GiftyAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
